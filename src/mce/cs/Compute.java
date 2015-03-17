e17feb68933b2a6c371964a25f3b99a029eaf0de
Shravanthi UM <shravanthium111@gmail.com>
2015-03-17 23:17:29 +0530

    override probability for byes theorem when two variable are taken at a time

--- src/mce/cs/Compute.java
+++ src/mce/cs/Compute.java
@@ -22,6 +22,25 @@ public class Compute {
 		return frequency;
 	}
 	
+	public static Map bayes_freq(VectorRep var,VectorRep clasLabel){
+		
+		Map map = new HashMap();
+		Enumeration enum1 = var.v.elements();
+		Set set = new HashSet((Collection) enum1);
+		while(set.iterator().hasNext()){
+			map.put(set.iterator().next(), new VectorRep());
+		}
+		Enumeration enum2 = clasLabel.v.elements();
+		for(int i=0; i<clasLabel.v.size();i++){
+			VectorRep vec = (VectorRep) map.get(enum1.nextElement());
+			int key= (int) enum2.nextElement();
+			vec.v.add(key);
+			map.put(enum1, vec);
+		}
+		return map;
+
+	}
+
 	public static double entropy(VectorRep values){
 		
 		double result = 0;
@@ -34,12 +53,20 @@ public class Compute {
 		
 	}
 	
+	
 	public static double gain(List <VectorRep> values){
 		double result = 0;
 		double label_entropy = entropy(values.get(values.size()-1));
+		
 		double gain=0;
 		for(int i=0; i<values.size()-1;i++){
-			double g = label_entropy -(double) entropy(values.get(i));
+			Map map = bayes_freq(values.get(i),values.get(values.size()-1));
+			double g = 0;
+			List freq = probability(values.get(i));
+			for(Object seq: map.keySet()){
+				double item = (double) freq.iterator().next();
+				g	-= label_entropy -(double) entropy((VectorRep)map.get(seq));
+			}
 			System.out.print((i+1)+" : "+g+"\n");
 			if (g>gain){
 				gain =g;
